cae652edfc2f3442c3103404b59b3d28548f8cfa
liujie <liujie@powerallnetworks.com>
2014-04-21 15:56:15 +0800

    update plugin

--- www/uploadanddownloadPlugin.js
+++ www/uploadanddownloadPlugin.js
@@ -6,12 +6,10 @@ var uploadAndDownloadPlugin = function() {
 };
 uploadAndDownloadPlugin.prototype.upload = function(resourcePath)
 {
-	alert("upload");
     exec(null, null, 'UploadAndDownloadPlugin', 'upload', [resourcePath]);
 };
 uploadAndDownloadPlugin.prototype.download = function(downloadPath)
 {
-	alert("download");
     exec(null, null, 'UploadAndDownloadPlugin', 'download', [downloadPath]);
 };
 var upAndDown = new uploadAndDownloadPlugin();
